0588c82dfd3d5300c73d0b438d3cd49b0a4fd633
rogeriocassares <roger10cassares@gmail.com>
2023-06-17 22:31:52 -0300

    stage 4 server

--- src/server.ts
+++ src/server.ts
@@ -3,21 +3,32 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+
 app.register(cors, {
-  // origin: true,
+  origin: true,
   // origin: ['http://localhost:3000', 'http://rocketseat.com.br/'],
-  origin: ['http://localhost:3000'],
+  // origin: ['http://localhost:3000'],
 })
 
 app.register(jwt, {
   secret: 'spacetime',
 })
 
+app.register(uploadRoutes)
 app.register(authRoutes)
 app.register(memoriesRoutes)
 
